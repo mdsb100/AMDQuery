281678ac6f62582607fd1772809e00b4d5e9653b
caojun <78612846@qq.com>
2012-10-16 23:36:21 +0800

    nothing

--- myquery/event/mouseinteraction.js
+++ myquery/event/mouseinteraction.js
@@ -1,39 +1,39 @@
-myQuery.define("event/mouseinteraction", ["main/event"], function($, event) {
-	function mouseinteraction(e,temp) {
-		switch(e.type) {
-		case "mousedown":
-		case "touchstart":
-			temp.isDown = 1;
-			event.trigger(temp.target,"mouseinteractiondown",this,e);
-			break;
-		case "mousemove":
-			if(e.which === 0 || (client.browser.ie678 && e.button != 1) || temp.isDown == 0) {
-                self.isDown = 0;
-                event.trigger(temp.target,"mouseinteractionmove",this,e);
+myQuery.define("event/mouseinteraction", ["main/event"], function ($, event) {
+    function mouseinteraction(e, temp) {
+        switch (e.type) {
+            case "mousedown":
+            case "touchstart":
+                temp.isDown = 1;
+                event.trigger(temp.target, "mouseinteractiondown", this, e);
                 break;
-            }
-		case "touchmove":
-			event.trigger(temp.target,"mouseinteractiondrag",this,e);
-			break;
-		case "mouseup":
-		case "touchend":
-			temp.isDown = 0;
-			event.trigger(temp.target,"mouseinteractionup",this,e);
-			break;
-		}
-	}
+            case "mousemove":
+                if (e.which === 0 || (client.browser.ie678 && e.button != 1) || temp.isDown == 0) {
+                    self.isDown = 0;
+                    event.trigger(temp.target, "mouseinteractionmove", this, e);
+                    break;
+                }
+            case "touchmove":
+                event.trigger(temp.target, "mouseinteractiondrag", this, e);
+                break;
+            case "mouseup":
+            case "touchend":
+                temp.isDown = 0;
+                event.trigger(temp.target, "mouseinteractionup", this, e);
+                break;
+        }
+    }
 
-	$.fn.extend {
-		mouseinteraction: function(down,drag,up,move) {
-			return this.each(function(ele) {
-				var temp = {
-					isDown: 0,
-					target: ele
-				},
-				anonymous = $.data(ele, "mouseinteraction", function(e) {
-					mouseinteraction.call(this, e, temp)
+    $.fn.extend({
+        mouseinteraction: function (down, drag, up, move) {
+            return this.each(function (ele) {
+                var temp = {
+                    isDown: 0,
+                    target: ele
+                },
+				anonymous = $.data(ele, "mouseinteraction", function (e) {
+				    mouseinteraction.call(this, e, temp);
 				});
-				event
+                event
 				.on(ele, "mouseinteractiondown", down)
 				.on(ele, "mouseinteractiondrag", drag)
 				.on(ele, "mouseinteractionup", up)
@@ -41,12 +41,12 @@ myQuery.define("event/mouseinteraction", ["main/event"], function($, event) {
 				.on(ele, "mousedown", anonymous)
 				.on(ele, "mousemove", anonymous)
 				.on(ele, "mouseup", anonymous);
-			});
-		},
-		unMouseinteraction: function(down,drag,up,move) {
-			return this.each(function(ele) {
-				var anonymous = $.data(ele, "mouseinteraction");
-				event
+            });
+        },
+        unMouseinteraction: function (down, drag, up, move) {
+            return this.each(function (ele) {
+                var anonymous = $.data(ele, "mouseinteraction");
+                event
 				.off(ele, "mouseinteractiondown", down)
 				.off(ele, "mouseinteractiondrag", drag)
 				.off(ele, "mouseinteractionup", up)
@@ -54,9 +54,9 @@ myQuery.define("event/mouseinteraction", ["main/event"], function($, event) {
 				.off(ele, "mousedown", anonymous)
 				.off(ele, "mousemove", anonymous)
 				.off(ele, "mouseup", anonymous);
-				$.data(ele, "mouseinteraction", null);
-			});
-		}
-	}
-	return mouseinteraction;
+                $.data(ele, "mouseinteraction", null);
+            });
+        }
+    });
+    return mouseinteraction;
 });
