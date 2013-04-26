d58678c942401a638715406a0f76892b4f7a9c68
caojun <78612846@qq.com>
2013-04-26 22:30:59 +0800

    fix draggable

--- myquery/ui/draggable.js
+++ myquery/ui/draggable.js
@@ -1,9 +1,9 @@
-﻿myQuery.define("ui/draggable", ["module/Widget", "main/event", "main/dom", "html5/css3", "main/query"], function ($, Widget, event, dom, cls3, query, undefined) {
+﻿myQuery.define("ui/draggable", ["module/Widget", "main/event", "main/dom", "html5/css3", "main/query"], function($, Widget, event, dom, cls3, query, undefined) {
     "use strict"; //启用严格模式
     var eventFuns = event.event.document,
         draggable = Widget.extend("ui.draggable", {
             container: null,
-            create: function () {
+            create: function() {
                 var self = this;
 
                 this.container.css("overflow", "hidden");
@@ -19,34 +19,37 @@
                 return this;
             },
             customEventName: ["start", "move", "stop", "pause"],
-            event: function () { },
-            enable: function () {
+            event: function() {},
+            enable: function() {
                 var fun = this.event;
                 this.disable();
                 $("body").on("mouseup", fun);
                 this.container.on('mousemove mouseup', fun);
                 this.target.on('mousedown', fun);
             },
-            disable: function () {
+            disable: function() {
                 var fun = this.event;
                 $("body").off("mouseup", fun);
                 this.container.off('mousemove mouseup', fun);
                 this.target.off('mousedown', fun);
             },
-            init: function (opt, target) {
+            init: function(opt, target) {
                 this._super(opt, target);
                 target.attr("myquery-ui", "draggable");
                 this.container = $(this.options.container || document.body);
                 return this.create().render();
             },
-            initPositionParent: function(){
+            initPositionParent: function() {
                 var result;
-                if(this.isTransform3d()){
+                if (this.isTransform3d()) {
                     this.container.initTransform3d();
-                    this.positionParent = this.container;
-                }
-                else{
-                    this.target.ancestors().each(function (ele) {
+                    if (this.options.container) {
+                        this.positionParent = this.container;
+                    } else {
+                        this.positionParent = this.target.parent();
+                    }
+                } else {
+                    this.target.ancestors().each(function(ele) {
                         switch ($.style(ele, "position")) {
                             case "absolute":
                             case "relative":
@@ -61,12 +64,25 @@
                     }
                     //self.container = $(result);
                     this.positionParent = $(result);
-                    if (this.options.overflow == true) {
-                        this.positionParent.css("overflow", "hidden");
-                    }
+                    this._setOverflow()
                 }
 
                 return this;
+            },
+            _setOverflow: function(overflow) {
+                if (overflow !== undefined) {
+                    this.options.overflow = overflow;
+                }
+                if (overflow == true) {
+                    this.positionParent.css("overflow", "hidden");
+                } else {
+                    this.positionParent.css("overflow", "");
+                }
+            },
+            _setContainer: function(container) {
+                if (this.options.container == null) {
+                    this.options.container = container;
+                }
             },
             options: {
                 container: null,
@@ -79,26 +95,35 @@
                 cursor: 'default',
                 overflow: false,
                 keepinner: true,
-                stopPropagation:true,
-                isTransform3d:false,
-                removeContainer: 0,
+                stopPropagation: true,
+                isTransform3d: false,
                 pauseSensitivity: 500
             },
+            setter: {
+                diffx: 0,
+                diffy: 0,
+                axisx: 0,
+                axisy: 0,
+            },
             public: {
-                getPositionX:1,
-                getPositionY:1,
-                render:1
+                getPositionX: 1,
+                getPositionY: 1,
+                render: 1
+            },
+            returns: {
+                getPositionX: 1,
+                getPositionY: 1
             },
             isTransform3d: function() {
                 return this.options.isTransform3d && $.support.transform3d;
             },
-            getPositionX: function(){
-                return this.target.getLeft() + (this.isTransform3d() ? this.target.transform3d("translateX", true): 0);
+            getPositionX: function() {
+                return this.target.getLeft() + (this.isTransform3d() ? this.target.transform3d("translateX", true) : 0);
             },
-            getPositionY: function(){
-                return this.target.getTop()  + (this.isTransform3d() ? this.target.transform3d("translateY", true): 0);
+            getPositionY: function() {
+                return this.target.getTop() + (this.isTransform3d() ? this.target.transform3d("translateY", true) : 0);
             },
-            _initHandler: function () {
+            _initHandler: function() {
                 var self = this,
                     target = self.target,
                     opt = self.options,
@@ -106,8 +131,8 @@
                     parentLeft = null,
                     parentTop = null,
                     dragging = null;
-                this.event = function (e) {
-                    if(e.type !== "mousemove" || dragging){
+                this.event = function(e) {
+                    if (e.type !== "mousemove" || dragging) {
                         var offsetLeft = self.getPositionX(),
                             offsetTop = self.getPositionY(),
                             x = e.pageX || e.clientX,
@@ -162,7 +187,7 @@
                             if (dragging !== null) {
                                 var
                                 con = self.container,
-                                cP;
+                                    cP;
                                 x -= (opt.diffx + parentLeft);
                                 y -= (opt.diffy + parentTop);
 
@@ -181,7 +206,7 @@
                                 target.trigger(para.type, target[0], para);
 
                                 clearTimeout(timeout);
-                                timeout = setTimeout(function () {
+                                timeout = setTimeout(function() {
                                     para.type = "drag.pause";
                                     target.trigger(para.type, target[0], para);
                                 }, opt.pauseSensitivity)
@@ -202,42 +227,43 @@
                 };
 
             },
-            render: function (x, y) {
-                return this.isTransform3d() ? this._renderByTransform3d(x, y): this._render(x, y);
+            render: function(x, y) {
+                return this.isTransform3d() ? this._renderByTransform3d(x, y) : this._render(x, y);
             },
-            _render: function(x, y){
+            _render: function(x, y) {
                 var opt = this.options;
-                
-                if(opt.axisx === true && x != undefined){
-                    this.target.css("left",x + "px");
+
+                if (opt.axisx === true && x != undefined) {
+                    this.target.css("left", x + "px");
                 }
-                if(opt.axisy === true && y != undefined){
-                    this.target.css("top",y + "px");
+                if (opt.axisy === true && y != undefined) {
+                    this.target.css("top", y + "px");
                 }
 
-                return this; 
+                return this;
             },
-            _renderByTransform3d: function(x, y){
-                var opt = this.options, obj ={};
-                
-                if(opt.axisx === true && x != undefined){
+            _renderByTransform3d: function(x, y) {
+                var opt = this.options,
+                    obj = {};
+
+                if (opt.axisx === true && x != undefined) {
                     obj.tx = x;
                 }
-                if(opt.axisy === true && y != undefined){
+                if (opt.axisy === true && y != undefined) {
                     obj.ty = y;
                 }
                 this.target.setTranslate3d(obj);
                 return this;
             },
             target: null,
-            toString: function () {
+            toString: function() {
                 return "ui.draggable";
             },
             widgetEventPrefix: "drag"
         });
 
     //提供注释
-    $.fn.draggable = function (a, b, c, args) {
+    $.fn.draggable = function(a, b, c, args) {
         /// <summary>使对象的第一元素可以拖动
         /// <para>bol obj.disabled:事件是否可用</para>
         /// <para>num obj.axis:"x"表示横轴移动;"y"表示纵轴移动;缺省或其他值为2轴</para>
@@ -251,8 +277,7 @@
         /// <param name="c" type="any">属性option子属性名的值</param>
         /// <param name="args" type="any">在调用方法的时候，后面是方法的参数</param>
         /// <returns type="$" />
-        draggable.apply(this, arguments);
-        return this;
+        return draggable.apply(this, arguments);
     }
 
     return draggable;
