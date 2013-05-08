e126d44bd81d9bd18c3ea3d15f8fe7176c4d3c5b
caojun <78612846@qq.com>
2013-05-08 22:37:18 +0800

    add object into json of main and change CustomEvent define by object.extend

--- myquery/json/package.js
+++ myquery/json/package.js
@@ -1,5 +1,4 @@
-﻿/// <reference path="../myquery.js" />
-myQuery.define("json/package", function($, undefinded) {
+﻿myQuery.define("json/package", function($, undefinded) {
 	"use strict"; //启用严格模式
 	var main = ["main/data", "main/attr", "main/class", "main/communicate", "main/CustomEvent", "main/dom", "main/event", "main/object", "main/query"],
 		html5 = ["html5/animate.transform", "html5/applicationCache", "html5/canvas.extend", "html5/css3", "html5/css3.transition.animate", "html5/Storage", "html5/threed.canvas.extend", "html5/Worker"];
--- myquery/main/CustomEvent.js
+++ myquery/main/CustomEvent.js
@@ -1,28 +1,29 @@
-﻿/// <reference path="../myquery.js" />
-
-myQuery.define("main/CustomEvent", function ($, undefined) {
+﻿myQuery.define("main/CustomEvent", ["main/object"], function($, object, undefined) {
     "use strict"; //启用严格模式
-
-    var proto = {
-        constructor: CustomEvent
-        , init: function () {
+    var CustomEvent = object.extend("CustomEvent", {
+        constructor: CustomEvent,
+        init: function() {
             this.handlers = {};
             this._handlerMap = {};
             return this;
-        }
-        , addHandler: function (type, handler) {
+        },
+        on: function(type, handler){
+            return this.addHandler(type, handler);
+        },
+        addHandler: function(type, handler) {
             /// <summary>添加自定义事件</summary>
             /// <para>例:"do undo"</para>
             /// <param name="type" type="String">方法类型</param>
             /// <param name="handler" type="Function">方法</param>
             /// <returns type="self" />
-            var types = type.split(" "), i = types.length - 1;
+            var types = type.split(" "),
+                i = types.length - 1;
             for (; i >= 0; i--) {
                 this._addHandler(types[i], handler);
             };
             return this;
-        }
-        , _addHandler: function(type, handler){
+        },
+        _addHandler: function(type, handler) {
             /// <summary>添加自定义事件</summary>
             /// <param name="type" type="String">方法类型</param>
             /// <param name="handler" type="Function">方法</param>
@@ -30,32 +31,39 @@ myQuery.define("main/CustomEvent", function ($, undefined) {
             var handlers = this._nameSpace(type);
             this.hasHandler(type, handler, handlers) == -1 && handlers.push(handler);
             return this;
-        }
-        , clearHandlers: function (type) {
+        },
+        clear: function(type){
+            return this.clearHandlers(type);
+        },
+        clearHandlers: function(type) {
             /// <summary>清楚所有自定义事件</summary>
             /// <returns type="self" />
             if (type) {
-                var types = type.split(" "), i = types.length - 1, item;
+                var types = type.split(" "),
+                    i = types.length - 1,
+                    item;
                 for (; i >= 0; i--) {
                     item = types[i];
                     this._nameSpace(item, true);
                     delete this._handlerMap[item];
                     delete this.handlers[item];
-                } 
-            }
-            else {
+                }
+            } else {
                 this.handlers = {};
             }
             return this;
-        }
-        , hasHandler: function (type, handler, handlers) {
+        },
+        hasHandler: function(type, handler, handlers) {
             /// <summary>是否有这个事件</summary>
             /// <para>返回序号 -1表示没有</para>
             /// <param name="type" type="String">方法类型</param>
             /// <param name="handler" type="Function">方法</param>
             /// <param name="handlers" type="Array/undefinded">已有的事件集</param>
             /// <returns type="Number" />
-            var handlers = handlers || this._nameSpace(type), i = 0, j = -1, len;
+            var handlers = handlers || this._nameSpace(type),
+                i = 0,
+                j = -1,
+                len;
             if (handlers instanceof Array && handlers.length) {
                 for (len = handlers.length; i < len; i++) {
                     if (handlers[i] === handler) {
@@ -65,8 +73,8 @@ myQuery.define("main/CustomEvent", function ($, undefined) {
                 }
             }
             return j;
-        }
-        , trigger: function (type, target, obj) {
+        },
+        trigger: function(type, target, obj) {
             /// <summary>配置自定义事件</summary>
             /// <param name="target" type="Object">当前对象</param>
             /// <param name="obj" type="Object">作用域</param>
@@ -74,44 +82,50 @@ myQuery.define("main/CustomEvent", function ($, undefined) {
             var handlers = this._nameSpace(type);
             if (handlers instanceof Array && handlers.length) {
                 for (var i = 0, len = handlers.length, arg = $.util.argToArray(arguments, 2); i < len; i++)
-                    handlers[i].apply(target, arg);
+                handlers[i].apply(target, arg);
             }
             return this;
-        }
-        , removeHandler: function(type, handler){
+        },
+        off: function(type, handler){
+            return this.removeHandler(type, handler);
+        },
+        removeHandler: function(type, handler) {
             /// <summary>移除自定义事件</summary>
             /// <para>例:"do undo"</para>
             /// <param name="type" type="String">方法类型</param>
             /// <param name="handler" type="Function">方法</param>
             /// <returns type="self" />
-            var types = type.split(" "), i = types.length - 1;
+            var types = type.split(" "),
+                i = types.length - 1;
             for (; i >= 0; i--) {
                 this._removeHandler(types[i], handler);
             };
             return this;
-        }
-        , _removeHandler: function (type, handler) {
+        },
+        _removeHandler: function(type, handler) {
             /// <summary>移除自定义事件</summary>
             /// <param name="type" type="String">方法类型</param>
             /// <param name="handler" type="Function">方法</param>
             /// <returns type="self" />
-            var handlers = this._nameSpace(type), i = this.hasHandler(type, handler, handlers);
+            var handlers = this._nameSpace(type),
+                i = this.hasHandler(type, handler, handlers);
             if (i > -1) {
                 handlers.splice(i, 1);
             }
-            return this;            
-        }
-        , _nameSpace: function (type, re) {
-            var nameList = type.split("."), result = this._initSpace(nameList, this.handlers, re);
+            return this;
+        },
+        _nameSpace: function(type, re) {
+            var nameList = type.split("."),
+                result = this._initSpace(nameList, this.handlers, re);
             //, i = 0, nameSpace, name, result;
             //nameList.length > 2 && tools.error({ fn: "CustomEvent._nameSpace", msg: "nameSpace is too long" });
 
             this._handlerMap[type] || (this._handlerMap[type] = result);
             return result;
-        }
-        , _initSpace: function (nameList, nameSpace, re) {
+        },
+        _initSpace: function(nameList, nameSpace, re) {
             var name = nameList[0],
-            result;
+                result;
             //name = nameList[1];
             if (nameSpace) {
                 result = nameSpace[name];
@@ -127,19 +141,7 @@ myQuery.define("main/CustomEvent", function ($, undefined) {
             nameList.splice(0, 1);
             return nameList.length ? this._initSpace(nameList, nameSpace, re) : result;
         }
-    };
-
-    function CustomEvent() {
-        /// <summary>自定义事件基类</summary>
-        /// <returns type="CustomEvent" />
-        //quote from written by Nicholas C.Zakas
-        this.init.apply(this, arguments);
-    }
-    CustomEvent.prototype = proto;
-
-    proto.on = proto.addHandler;
-    proto.off = proto.removeHandler;
-    proto.clear = proto.clearHandlers;
+    });
 
     return $.CustomEvent = CustomEvent;
 });
