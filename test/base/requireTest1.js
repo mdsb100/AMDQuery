b12707b9e9c29814e14f9a337b09871da421f631
caojun <78612846@qq.com>
2013-07-27 10:06:22 +0800

    fix require: variable path

--- test/base/requireTest1.js
+++ test/base/requireTest1.js
@@ -1,4 +1,4 @@
-myQuery.define("$variable/base/requireTest1", ["app/Controller", "$variable/base/requireTest2"], function($, Controller, requireTest2, undefined) {
+myQuery.define("@variable/base/requireTest1", ["app/Controller", "@variable/base/requireTest2"], function($, Controller, requireTest2, undefined) {
     "use strict"; //启用严格模式
 
     return {};
