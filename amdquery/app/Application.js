f4b8baa72f9315e097f3e02330d55ecd537d9e21
caojun <78612846@qq.com>
2013-08-14 18:11:11 +0800

    modify mvc

--- amdquery/app/Application.js
+++ amdquery/app/Application.js
@@ -1,4 +1,4 @@
-aQuery.define( "app/Application", [ "base/promise", "base/typed", "main/attr", "main/CustomEvent", "main/query", "main/object", "app/Model", "app/View", "app/Controller", "ecma5/array.compati" ], function( $, Promise, typed, attr, CustomEvent, query, object, BaseModel, BaseView, BaseController, Array, undefined ) {
+aQuery.define( "app/Application", [ "base/ClassModule", "base/promise", "base/typed", "main/attr", "main/CustomEvent", "main/query", "main/object", "app/Model", "app/View", "app/Controller", "ecma5/array.compati" ], function( $, ClassModule, Promise, typed, attr, CustomEvent, query, object, BaseModel, BaseView, BaseController, Array, undefined ) {
   "use strict"; //启用严格模式
   var defaultViewSrc = "app/View";
 
@@ -22,8 +22,10 @@ aQuery.define( "app/Application", [ "base/promise", "base/typed", "main/attr", "
   var Application = object.extend( "Application", {
     init: function( promiseCallback ) {
       this._super( );
-      this.ready = new Promise( );
-      this.__promiseCallback = promiseCallback;
+      // this.ready = new Promise( );
+      // this.__promiseCallback = promiseCallback;
+      this.index = null;
+      this.load( );
     },
     getAppRelativePath: function( path ) {
       if ( path ) {
@@ -33,96 +35,63 @@ aQuery.define( "app/Application", [ "base/promise", "base/typed", "main/attr", "
         return "";
       }
     },
-    _findViewElement: function( parent ) {
-      var children = query.find( ">*", parent ),
-        viewElements = [ ],
-        lave = [ ],
-        ele,
-        i = 0;
+    // _findViewElement: function( parent ) {
+    //   var children = query.find( ">*", parent ),
+    //     viewElements = [ ],
+    //     lave = [ ],
+    //     ele,
+    //     i = 0;
 
-      for ( i = children.length - 1; i >= 0; i-- ) {
-        ele = children[ i ];
-        if ( typed.isNode( ele, "View" ) ) {
-          viewElements.push( ele );
-        } else {
-          lave.push( ele );
-        }
-      }
+    //   for ( i = children.length - 1; i >= 0; i-- ) {
+    //     ele = children[ i ];
+    //     if ( typed.isNode( ele, "View" ) ) {
+    //       viewElements.push( ele );
+    //     } else {
+    //       lave.push( ele );
+    //     }
+    //   }
 
 
-      for ( i = lave.length - 1; i >= 0; i-- ) {
-        ele = lave[ i ];
-        viewElements = viewElements.concat( this._findViewElement( ele ) );
-      }
+    //   for ( i = lave.length - 1; i >= 0; i-- ) {
+    //     ele = lave[ i ];
+    //     viewElements = viewElements.concat( this._findViewElement( ele ) );
+    //   }
 
-      return viewElements;
-    },
+    //   return viewElements;
+    // },
     load: function( ) {
-      var self = this;
+      // var self = this;
 
-      var ready = this.ready,
-        eles = this._findViewElement( document.body );
+      // var ready = this.ready;
+      if ( !this.index ) {
+        this.index = Application.loadController( document.body, "Index" );
+      }
+      // ready.then( function( ) {
+      //   self.launch( );
+      //   self.__promiseCallback.resolve( );
+      //   delete self.__promiseCallback;
+      // } );
 
-      eles.forEach( function( element ) {
-        //注意销毁
-        var viewSrc = self.getAppRelativePath( attr.getAttr( element, "src" ) ) || defaultViewSrc;
-        var controllerSrc = self.getAppRelativePath( attr.getAttr( element, "controller" ) || getControllerSrcByViewSrc( viewSrc ) );
-
-        ready = ready.then( function( ) {
-
-        } );
-
-        // ready = ready.then( function( ) {
-        //   var promise = this;
-        //   require( viewSrc, function( View ) {
-        //     promise.resolve( BaseView.getInstance( element, View ) );
-        //   } );
-        //   return promise;
-        // } ).then( function( view ) {
-        //   var promise = this;
-
-        //   var modelsSrc = view._getModelsSrc( ).map( function( src ) {
-        //     return self.getAppRelativePath( src );
-        //   } );
-
-        //   var modelsElement = view._getModelsElement( );
-
-        //   if ( modelsSrc.length ) {
-        //     require( modelsSrc, function( ) {
-        //       var models = $.util.argToArray( arguments ).map( function( Model, index ) {
-        //         return BaseModel.getInstance( modelsElement[ index ], Model );
-        //       } );
-        //       promise.resolve( view );
-        //     } );
-        //     return promise;
-        //   } else {
-        //     return view;
-        //   }
-        // } ).then( function( view ) {
-        //   var promise = this;
-        //   require( controllerSrc, function( Controller ) {
-        //     promise.resolve( BaseController.getInstance( element, Controller ) );
-        //   } );
-        //   return promise;
-        // } );
-
-      } );
-
-      ready.then( function( ) {
-        self.launch( );
-        self.__promiseCallback.resolve( );
-        delete self.__promiseCallback;
-      } );
-
-      ready.rootResolve( );
+      // ready.rootResolve( );
     },
 
     launch: function( ) {
 
-      return self.trigger( "launch", self );
     }
   }, {
+    loadController: function( container, tagName ) {
+      var contollerElement = query.find( tagName || "Require" ),
+        self = this;
 
+      if ( contollerElement[ 0 ] ) {
+        var src = attr.getAttr( contollerElement, "src" );
+        if ( ClassModule.contains( src ) ) {
+          require.sync( );
+        }
+        return new require( src ).first;
+      }
+      return null;
+    }
   }, CustomEvent );
 
   return Application;
--- test/app/app.js
+++ test/app/app.js
@@ -1,11 +1,12 @@
 aQuery.define( "@app/app", [ "app/Application" ], function( $, Application ) {
   "use strict"; //启用严格模式
+  //必须依赖index controller
   var app = Application.extend( "Application", {
     init: function( promiseCallback ) {
       this._super( promiseCallback );
     },
     launch: function( ) {
-
+      // this.index.doSomething
     }
   }, {
 
