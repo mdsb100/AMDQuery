a584233a5987bca5847f2395f69704657f0ec807
caojun <78612846@qq.com>
2013-08-14 20:11:52 +0800

    modify View

--- amdquery/app/View.js
+++ amdquery/app/View.js
@@ -1,11 +1,11 @@
-aQuery.define( "app/View", [ "main/query", "main/object", "main/attr", "main/CustomEvent", "module/Widget", ], function( $, query, object, attr, CustomEvent, Widget, undefined ) {
+aQuery.define( "app/View", [ "base/ClassModule", "main/communicate", "main/query", "main/object", "main/attr", "main/CustomEvent", "module/Widget", ], function( $, ClassModule, communicate, query, object, attr, CustomEvent, Widget, undefined ) {
   "use strict"; //启用严格模式
   var View = object.extend( "View", {
-    init: function( ViewElement ) {
+    init: function( ) {
       this._super( );
-      this.element = ViewElement;
+      this.topElement = View.getHTML( );
       // this.src = viewSrc;
-      this.id = attr.getAttr( ViewElement, "id" ) || null;
+      this.id = attr.getAttr( this.topElement, "id" ) || null;
       //不能有相同的两个src
 
       View.addView( this );
@@ -13,7 +13,7 @@ aQuery.define( "app/View", [ "main/query", "main/object", "main/attr", "main/Cus
     _getModelsElement: function( ) {
       //可能会错 找直接子元素
       //Collection
-      return query.find( ">Model", this.element );
+      return query.find( ">Model", this.topElement );
     },
     _getModelsSrc: function( ) {
       return this._getModelsElement( ).map( function( ele ) {
@@ -29,12 +29,40 @@ aQuery.define( "app/View", [ "main/query", "main/object", "main/attr", "main/Cus
     },
     destory: function( ) {
       View.removeView( this );
+    },
+    htmlSrc: "",
+    _timeout: 5000,
+    _error: function( ) {
+      $.console.error( "get " + this.htmlSrc + " error" );
+    },
+    getHtml: function( ) {
+      if ( !ClassModule.contains( this.htmlSrc ) ) {
+        if ( this.htmlSrc === "" ) {
+          define( this.htmlSrc, function( ) {
+            return document.createElement( "div" );
+          } );
+        } else {
+          var self = this,
+            element;
+          communicate.ajax( {
+            url: this.htmlSrc,
+            async: false,
+            dataType: "xml",
+            complete: function( xml ) {
+              define( this.htmlSrc, xml );
+            },
+            timeout: View.timeout,
+            timeoutFun: View.error
+          } );
+        }
+      }
+      return require( this.htmlSrc ).first.cloneNode( );
     }
   }, {
     views: [ ],
-    getInstance: function( viewElement, ViewObject ) {
-      return ViewObject ? new ViewObject( viewElement ) : new View( viewElement );
-    },
+    // getInstance: function( viewElement, ViewObject ) {
+    //   return ViewObject ? new ViewObject( viewElement ) : new View( viewElement );
+    // },
     getView: function( id ) {
       var result;
       this.views.forEach( function( view ) {
@@ -58,9 +86,6 @@ aQuery.define( "app/View", [ "main/query", "main/object", "main/attr", "main/Cus
   }, CustomEvent );
 
   object.providePropertyGetSet( View, {
-    element: "-pu -r",
-    // src: "-pu -r",
-    modelsSrc: "-pu -r",
     id: "-pu -r"
   } );
 
