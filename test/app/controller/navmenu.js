27059fbe92c2ee1bc11a6e57f5a7d94ad13ba04a
caojun <78612846@qq.com>
2013-08-20 23:02:24 +0800

    modify mvc

---
+++ test/app/controller/navmenu.js
@@ -0,0 +1,16 @@
+aQuery.define( "@app/controller/navmenu", [ "app/Controller", "@app/view/navmenu" ], function( $, SuperController, NavmenuView, undefined ) {
+  "use strict"; //启用严格模式
+  var Controller = SuperController.extend( {
+    init: function( id, contollerElement ) {
+      this._super( id, contollerElement, IndexView );
+
+    },
+    onReady: function( ) {
+      console.log( "navmenu load" );
+    }
+  }, {
+
+  } );
+
+  return Controller;
+} );
