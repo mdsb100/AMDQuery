53528b689741d092a8c06bf65f6ee6d25de64a2d
caojun <78612846@qq.com>
2013-09-20 15:55:25 +0800

    change document-view

--- document/app/view/index.js
+++ document/app/view/index.js
@@ -1,17 +1,12 @@
 aQuery.define( "@app/view/index", [ "app/View" ], function( $, SuperView, undefined ) {
   "use strict"; //启用严格模式
-  var htmlSrc = "@app/xml/index";
-
-  SuperView.getHtml( htmlSrc );
-
   SuperView.getStyle( "@app/css/reset" );
 
   var View = SuperView.extend( {
     init: function( contollerElement ) {
-      this._super( contollerElement );
+      this._super( contollerElement, "@app/xml/index" );
 
-    },
-    htmlSrc: htmlSrc
+    }
   }, {
 
   } );
