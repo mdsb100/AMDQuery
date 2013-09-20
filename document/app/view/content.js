b99e707ca912cdfec961ebb4423a9a20d9721f9f
caojun <78612846@qq.com>
2013-09-20 15:55:25 +0800

    change document-view

--- document/app/view/content.js
+++ document/app/view/content.js
@@ -1,15 +1,10 @@
 aQuery.define( "@app/view/content", [ "app/View" ], function( $, SuperView, undefined ) {
   "use strict"; //启用严格模式
-  var htmlSrc = "@app/xml/content";
-
-  SuperView.getHtml( htmlSrc );
-
   var View = SuperView.extend( {
     init: function( contollerElement ) {
-      this._super( contollerElement );
+      this._super( contollerElement, "@app/xml/content" );
 
-    },
-    htmlSrc: htmlSrc
+    }
   }, {
 
   } );
