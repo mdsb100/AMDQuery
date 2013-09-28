34dfd99bee22fe3a2ce19c73c2c892b31fb70ac1
caojun-mac <78612846@qq.com>
2013-09-28 18:01:10 +0800

    modify swapview

--- amdquery/ui/swapview.js
+++ amdquery/ui/swapview.js
@@ -1,4 +1,5 @@
 aQuery.define( "ui/swapview", [
+  "base/support",
   "main/query",
   "main/css",
   "main/position",
@@ -11,15 +12,65 @@ aQuery.define( "ui/swapview", [
   "ui/draggable",
   "module/Widget",
   "module/animate",
-  "module/tween.extend" ], function( $, query, css, position, dom, cls, css3, animateTransform, css3Transition, swappable, draggable, Widget, animate, tween, undefined ) {
+  "module/tween.extend" ], function( $, support, query, css, position, dom, cls, css3, animateTransform, css3Transition, swappable, draggable, Widget, animate, tween, undefined ) {
   "use strict"; //启用严格模式
+
+  var horizental = "H",
+    vertical = "V";
+
+  var isTransform3d = !! $.config.module.transitionToAnimation && support.transform3d;
   var swapview = Widget.extend( "ui.swapview", {
     container: null,
     create: function( ) {
       var opt = this.options;
 
+      this.target.uiSwappable( );
+
+      var isHorizental = opt.orientation === horizental;
+
+      this.$board = this.target.children( "ol" );
+
+      this.$board.css( {
+        dislplay: "block"
+      } ).uiDraggable( {
+        keepinner: 1,
+        innerWidth: 40,
+        innerHeight: 40,
+        axis: isHorizental ? "x" : "y",
+        stopPropagation: false,
+        axisx: isHorizental,
+        axisy: !isHorizental,
+        isTransform3d: isTransform3d,
+        container: this.target,
+        overflow: true
+      } );
+
+      this.$views = this.$board.children( "li" );
+
+      if ( isHorizental ) {
+        this.$views.css( "float", "left" );
+      } else {
+        this.$views.css( "clear", "left" );
+      }
+
+      this.resize();
 
       return this;
+    },
+    resize: function( ) {
+      var width = this.target.width( );
+      var height = this.target.height( );
+      this.$views.width( width );
+      this.$views.height( height );
+
+      if ( this.options.orientation === horizental ) {
+        this.$board.width( width * this.$views.length );
+        this.$board.height( height );
+      } else {
+        this.$board.height( height * this.$views.length );
+        this.$board.width( width );
+      }
+
     },
     event: function( ) {},
     enable: function( ) {
@@ -47,18 +98,25 @@ aQuery.define( "ui/swapview", [
       return this;
     },
     destory: function( key ) {
+      this.target.uiSwappable( "destory", key );
+      this.$board.uiDraggable( "destory", key );
       Widget.invoke( "destory", this, key );
     },
     init: function( opt, target ) {
       this._super( opt, target );
-
       return this.create( )._initHandler( ).enable( ).render( );
     },
     customEventName: [ ],
     options: {
-
+      orientation: horizental
     },
     publics: {
+
+    },
+    setter: {
+      orientation: Widget.initFirst
+    },
+    getter: {
 
     },
     render: function( ) {
