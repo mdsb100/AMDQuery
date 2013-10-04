1519b4a6dec1eedaa656bbb565429b1c6bc26fc2
caojun-mac <78612846@qq.com>
2013-10-04 22:53:23 +0800

    modify swapview

--- amdquery/ui/swapview.js
+++ amdquery/ui/swapview.js
@@ -6,13 +6,31 @@ aQuery.define( "ui/swapview", [
   "main/dom",
   "main/class",
   "html5/css3",
+  "html5/css3.position",
   "html5/animate.transform",
   "html5/css3.transition.animate",
   "ui/swappable",
   "ui/draggable",
   "module/Widget",
   "module/animate",
-  "module/tween.extend" ], function( $, support, query, css, position, dom, cls, css3, animateTransform, css3Transition, swappable, draggable, Widget, animate, tween, undefined ) {
+  "module/FX",
+  "module/tween.extend" ], function( $,
+  support,
+  query,
+  css,
+  position,
+  dom,
+  css2,
+  css3,
+  css3Position,
+  animateTransform,
+  css3Transition,
+  swappable,
+  draggable,
+  Widget,
+  animate,
+  FX,
+  tween, undefined ) {
   "use strict"; //启用严格模式
 
   var horizental = "H",
@@ -28,24 +46,25 @@ aQuery.define( "ui/swapview", [
 
       var isHorizental = opt.orientation === horizental;
 
-      this.$board = this.target.children( "ol" );
+      this.container = this.target.children( "ol" );
 
-      this.$board.css( {
+      this.target.uiSwappable( );
+
+      this.container.css( {
         dislplay: "block"
       } ).uiDraggable( {
         keepinner: 1,
-        innerWidth: 40,
-        innerHeight: 40,
+        innerWidth: this.target.width( ) / 4,
+        innerHeight: this.target.height( ) / 4,
         axis: isHorizental ? "x" : "y",
         stopPropagation: false,
         axisx: isHorizental,
         axisy: !isHorizental,
-        isTransform3d: isTransform3d,
         container: this.target,
         overflow: true
       } );
 
-      this.$views = this.$board.children( "li" );
+      this.$views = this.container.children( "li" );
 
       if ( isHorizental ) {
         this.$views.css( "float", "left" );
@@ -53,36 +72,123 @@ aQuery.define( "ui/swapview", [
         this.$views.css( "clear", "left" );
       }
 
-      this.resize();
+      this.resize( );
 
       return this;
+    },
+    appendView: function( ) {
+
+    },
+    removeView: function( ) {
+
+    },
+    insertBeforeView: function( ) {
+
     },
     resize: function( ) {
       var width = this.target.width( );
       var height = this.target.height( );
+      this.width = width;
+      this.height = height;
+
       this.$views.width( width );
       this.$views.height( height );
 
       if ( this.options.orientation === horizental ) {
-        this.$board.width( width * this.$views.length );
-        this.$board.height( height );
+        this.boardWidth = width * this.$views.length;
+        this.boardHeight = height;
+
+
       } else {
-        this.$board.height( height * this.$views.length );
-        this.$board.width( width );
+        this.boardWidth = height * this.$views.length;
+        this.boardHeight = width;
       }
 
+      this.container.width( this.boardWidth );
+      this.container.height( this.boardHeight );
     },
-    event: function( ) {},
+    render: function( index ) {
+      var opt = this.options,
+        self = this;
+      if ( index === undefined || index < 0 || index > this.$views.length || opt.index === index ) {
+        return;
+      }
+
+      opt.index = index;
+
+      var animationOpt;
+
+      if ( opt.orientation == horizental ) {
+        animationOpt = $.getPositionAnimationOptionProxy( isTransform3d, -this.width * index );
+      } else {
+        animationOpt = $.getPositionAnimationOptionProxy( isTransform3d, -this.height * index );
+      }
+      // this.traget.trigger()
+      //点击stop 然后继续 动画
+      var animationEvent = {
+        type: this.getEventName( "beforeAnimation" ),
+        target: this.container[ 0 ],
+        view: this.$views[ index ],
+        index: index
+      }
+      this.target.trigger( animationEvent.type, animationEvent.target, animationEvent );
+
+      this.container.animate( animationOpt, {
+        duration: opt.animationDuration,
+        easing: opt.animationEasing,
+        queue: false,
+        complete: function( ) {
+          animationEvent.type = "afterAnimation";
+          self.target.trigger( animationEvent.type, animationEvent.target, animationEvent );
+        }
+      } );
+    },
+    _setIndex: function( index ) {
+      this.render( index );
+    },
     enable: function( ) {
       var event = this.event;
-
+      this.container.on( "drag.pause drag.move drag.start", event );
+      this.target.on( "swap.move swap.stop swap.pause", event );
       this.options.disabled = true;
       return this;
     },
     disable: function( ) {
       var event = this.event;
-
+      this.container.off( "drag.pause drag.move drag.start", event );
+      this.target.off( "swap.move swap.stop swap.pause", event );
       this.options.disabled = false;
+      return this;
+    },
+    checkSwap: function( e ) {
+      var opt = this.options,
+        a0 = e.acceleration,
+        t0 = opt.animateDuration - e.duration,
+        s0 = Math.round( a0 * t0 * t0 * 0.5 ),
+        direction = e.direction;
+
+      if ( t0 <= 0 ) {
+        this.toYBoundary( this.getTop( ) ).toXBoundary( this.getLeft( ) );
+        return this.hideStatusBar( );
+      }
+
+      switch ( direction ) {
+        case 3:
+          this.toX( -s0, t0, direction );
+          break;
+        case 9:
+          this.toX( s0, t0, direction );
+          break;
+        case 6:
+          this.toY( -s0, t0, direction );
+          break;
+        case 12:
+          this.toY( s0, t0, direction );
+          break;
+        default:
+          this.toXBoundary( this.getTop( ) ).toYBoundary( this.getLeft( ) );
+      }
+
       return this;
     },
     _initHandler: function( ) {
@@ -91,37 +197,45 @@ aQuery.define( "ui/swapview", [
         opt = self.options;
 
       this.event = function( e ) {
-        // switch ( e.type ) {
+        switch ( e.type ) {
+          case "drag.stop":
 
-        // }
+            break;
+          case "swap.stop":
+            console.log( "!!!!" )
+            break;
+        }
       };
       return this;
     },
     destory: function( key ) {
       this.target.uiSwappable( "destory", key );
-      this.$board.uiDraggable( "destory", key );
+      this.container.uiDraggable( "destory", key );
       Widget.invoke( "destory", this, key );
     },
     init: function( opt, target ) {
       this._super( opt, target );
+      this.width = 0;
+      this.height = 0;
+      this.boardWidth = 0;
+      this.boardHeight = 0;
       return this.create( )._initHandler( ).enable( ).render( );
     },
-    customEventName: [ ],
+    customEventName: [ "beforeAnimation", "afterAnimation" ],
     options: {
-      orientation: horizental
+      index: 0,
+      orientation: horizental,
+      animationDuration: FX.normal,
+      animationEasing: "expo.easeInOut"
     },
     publics: {
-
+      render: Widget.AllowPublic
     },
     setter: {
       orientation: Widget.initFirst
     },
     getter: {
 
-    },
-    render: function( ) {
-
-      return this;
     },
     target: null,
     toString: function( ) {
