aa49e21308c489208983b804b8a7eb9f525bc061
caojun-mac <78612846@qq.com>
2013-10-06 21:07:39 +0800

    add swap indicator

---
+++ amdquery/ui/swapindicator.js
@@ -0,0 +1,12 @@
+aQuery.define( "ui/swapview", [
+  "base/support",
+  "main/query",
+  "main/css",
+  "main/position",
+  "main/dom",
+  "main/class",
+  "html5/css3",
+  "module/Widget",
+   ], function( $, support, query, css2, position, dom, cls, css3, Widget ) {
+
+} );
