1c31581d88c9647a67c7515d3252655b2948003a
caojun-mac <78612846@qq.com>
2013-10-07 16:08:25 +0800

    modify document

--- document/app/controller/index.js
+++ document/app/controller/index.js
@@ -7,12 +7,15 @@ aQuery.define( "@app/controller/index", [ "app/Controller", "@app/view/index" ],
     },
     onReady: function( ) {
       var self = this;
+
       this.navmenu.on( "navmenu.select", function( e ) {
         self.content.loadPath( e.path );
       } );
       this.navmenu.on( "navmenu.dblclick", function( e ) {
         self.content.openWindow( );
       } );
+
+      // this.navmenu.selectDefaultNavmenu();
     },
     onDestory: function( ) {
       this.navmenu.clearHandlers( );
