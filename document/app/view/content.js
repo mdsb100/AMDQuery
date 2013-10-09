bd6eb1928b2186dc0b3470da8825afa2af92efe8
caojun-mac <78612846@qq.com>
2013-10-09 23:33:31 +0800

    modify document

--- document/app/view/content.js
+++ document/app/view/content.js
@@ -1,9 +1,11 @@
-aQuery.define( "@app/view/content", [ "app/View" ], function( $, SuperView, undefined ) {
+aQuery.define( "@app/view/content", [ "base/client", "app/View" ], function( $, client, SuperView, undefined ) {
   "use strict"; //启用严格模式
   var View = SuperView.extend( {
     init: function( contollerElement ) {
       this._super( contollerElement, "@app/xml/content" );
-
+      if ( client.browser.ie ) {
+        $( this.topElement ).css( "height", "100%" );
+      }
     }
   }, {
 
--- document/app/view/navmenu.js
+++ document/app/view/navmenu.js
@@ -1,9 +1,11 @@
-aQuery.define( "@app/view/navmenu", [ "app/View" ], function( $, SuperView, undefined ) {
+aQuery.define( "@app/view/navmenu", [ "base/client", "main/css", "app/View" ], function( $, client, css, SuperView, undefined ) {
   "use strict"; //启用严格模式
   var View = SuperView.extend( {
     init: function( contollerElement ) {
       this._super( contollerElement, "@app/xml/navmenu" );
-
+      if ( client.browser.ie ) {
+        $( this.topElement ).css( "height", "100%" );
+      }
     },
     onDomReady: function( ) {
       SuperView.getStyle( "@app/css/navmenu" );
