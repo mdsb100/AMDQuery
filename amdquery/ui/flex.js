b8a2005af2c3f7c9b73a76f9f1317651b50e84b0
caojun-mac <78612846@qq.com>
2013-10-20 11:20:18 +0800

    modify flex without IE

--- amdquery/ui/flex.js
+++ amdquery/ui/flex.js
@@ -9,15 +9,16 @@ aQuery.define( "ui/flex", [
     "main/position",
     "main/dom",
     "main/attr",
-    "html5/css3"
+    "html5/css3",
+    "util/function.extend"
   ],
-  function( $, typed, support, Widget, query, cls, event, css, position, dom, attr, css3 ) {
+  function( $, typed, support, Widget, query, cls, event, css, position, dom, attr, css3, functionExtend ) {
     "use strict"; //启用严格模式
 
     Widget.fetchCSS( "ui/css/flex" );
 
     var domStyle = document.documentElement.style,
-      proto, flex,
+      proto,
       flexName = "",
       flexDirectionName = "";
 
@@ -41,22 +42,24 @@ aQuery.define( "ui/flex", [
         event: function( ) {},
         _initHandler: function( ) {
           // var self = this;
-          this.event = function( e ) {
 
-          };
           return this;
         },
         enable: function( ) {
-
           this.options.disabled = true;
           return this;
         },
         disable: function( ) {
-
           this.options.disabled = false;
           return this;
         },
-        render: function( ) {
+        render: function( width, height ) {
+          if ( !typed.isNul( width ) ) {
+            this.setWidth( width );
+          }
+          if ( !typed.isNul( height ) ) {
+            this.setHeight( height );
+          }
           var opt = this.options;
           if ( opt.flex !== originFlexValue ) {
             //fix css + px because this is original string
@@ -67,9 +70,12 @@ aQuery.define( "ui/flex", [
           }
           return this;
         },
-        resize: function( width, height ) {
-          typed.isNul( width ) && this.setWidth( width );
-          typed.isNul( height ) && this.setHeight( height );
+        findParent: function( ) {
+          var parent = this.target.parent( "[ui-flex]" );
+          if ( parent.length && parent[ 0 ] === this.target[ 0 ].parentNode ) {
+            return parent;
+          }
+          return null;
         },
         setWidth: function( width ) {
           this.target.width( width );
@@ -82,14 +88,24 @@ aQuery.define( "ui/flex", [
         },
         init: function( opt, target ) {
           this._super( opt, target );
-          this.target.addClass( 'flex' );
+          this.target.addClass( "flex" );
+          if ( !this.findParent( ) ) {
+            if ( this.options.fillParentWidth ) {
+              this.target.css( "width", "100%" );
+            }
+            if ( this.options.fillParentHeight ) {
+              this.target.css( "height", "100%" );
+            }
+          }
           this.render( );
           return this;
         },
         customEventName: [ ],
         options: {
           flex: originFlexValue,
-          flexDirection: originFlexDirectionValue
+          flexDirection: originFlexDirectionValue,
+          fillParentWidth: true,
+          fillParentHeight: true
         },
         getter: {
 
@@ -99,8 +115,7 @@ aQuery.define( "ui/flex", [
         },
         publics: {
           setWidth: Widget.AllowPublic,
-          setHeight: Widget.AllowPublic,
-          resize: Widget.AllowPublic
+          setHeight: Widget.AllowPublic
         },
         target: null,
         toString: function( ) {
@@ -120,39 +135,67 @@ aQuery.define( "ui/flex", [
         event: function( ) {},
         _initHandler: function( ) {
           // var self = this;
-          this.event = function( e ) {
+          var self = this;
 
-          };
+          this.event = functionExtend.debounce( function( e ) {
+            switch ( e.type ) {
+              case "resize":
+                self.fillParent( );
+                self.render( );
+                break;
+            }
+          }, 10 );
+
           return this;
         },
         enable: function( ) {
-
+          if ( !! this.findParent( ) ) {
+            event.event.document.addHandler( window, "resize", this.event );
+          }
           this.options.disabled = true;
           return this;
         },
         disable: function( ) {
-
+          if ( !! this.findParent( ) ) {
+            event.event.document.removeHandler( window, "resize", this.event );
+          }
           this.options.disabled = false;
           return this;
         },
-        render: function( ) {
+        render: function( width, height ) {
+
+          if ( !typed.isNul( width ) ) {
+            this.setWidth( width );
+          }
+          if ( !typed.isNul( height ) ) {
+            this.setHeight( height );
+          }
+
+          this.toDirection( this.options.flexDirection );
+
+          //来自父元素的
           if ( this._lock === false ) {
             this.noticeParent( );
           }
+
           return this;
         },
-        resize: function( width, height ) {
-          typed.isNul( width ) && this.target.width( width );
-          typed.isNul( height ) && this.target.height( height );
-          this.render( );
-        },
         setWidth: function( width ) {
-          this.target.width( width );
-          this.render( );
+          this.render( width, null );
         },
         setHeight: function( height ) {
-          this.target.height( height );
-          this.render( );
+          this.render( null, height );
+        },
+        _setFlexDirection: function( flexDirection ) {
+          var opt = this.options;
+          switch ( flexDirection ) {
+            case "row":
+              opt.flexDirection = flexDirection;
+              break;
+            case "column":
+              opt.flexDirection = flexDirection;
+              break;
+          }
         },
         _setFlex: function( flex ) {
           if ( typed.isNum( flex ) && flex >= 0 ) {
@@ -161,14 +204,137 @@ aQuery.define( "ui/flex", [
             }
           }
         },
+        filterFlex: function( ) {
+          var children = this.target.children( ),
+            $item,
+            flexTarget = [ ],
+            itemFlex,
+            totalFlex = 0,
+            $lastItem = null,
+            isFlex,
+            hasFlex,
+            traceWidth = this.width,
+            traceHeight = this.height;
+
+          children.each( function( ele ) {
+            $item = $( ele );
+            isFlex = $item.isWidget( "ui.flex" );
+            itemFlex = 0;
+            $item.isFlex = isFlex;
+            if ( isFlex ) {
+              $item.uiFlex( );
+              itemFlex = $item.uiFlex( "option", "flex" );
+            }
+            hasFlex = itemFlex > 0;
+            $item.hasFlex = hasFlex;
+            if ( isFlex ) {
+              if ( hasFlex ) {
+                totalFlex += itemFlex;
+                $lastItem = $item;
+              }
+            } else {
+              traceWidth -= $item.outerWidth( );
+              traceHeight -= $item.outerHeight( );
+            }
+            flexTarget.push( $item );
+          } );
+
+          traceWidth = Math.max( traceWidth, 0 );
+          traceHeight = Math.max( traceHeight, 0 );
+          if ( $lastItem ) {
+            $lastItem.isLastItem = true;
+          }
+          return {
+            flexTarget: flexTarget,
+            totalFlex: totalFlex,
+            traceWidth: traceWidth,
+            traceHeight: traceHeight
+          };
+        },
+        toDirection: function( direction ) {
+          var ret = this.filterFlex( ),
+            flexTarget = ret.flexTarget,
+            totalFlex = ret.totalFlex,
+            traceWidth = ret.traceWidth,
+            traceHeight = ret.traceHeight,
+            $item, i = 0,
+            len = flexTarget.length,
+            sumWidth = 0,
+            sumHeight = 0,
+            tempWidth = null,
+            tempHeight = null,
+            itemFlex,
+            isFillParentWidth = false,
+            isFillParentHeight = false;
+
+          for ( ; i < len; i++ ) {
+            $item = flexTarget[ i ];
+            $item.css( "display", "block" );
+            switch ( direction ) {
+              case "row":
+                $item.css( "float", "left" );
+                break;
+              case "column":
+                $item.css( "clear", "both" );
+                break;
+            }
+
+            if ( $item.isFlex ) {
+              tempWidth = null;
+              tempHeight = null;
+              isFillParentHeight = $item.uiFlex( "option", "fillParentHeight" );
+              isFillParentWidth = $item.uiFlex( "option", "fillParentWidth" );
+              itemFlex = $item.uiFlex( "option", "flex" );
+              if ( itemFlex > 0 ) {
+                if ( direction === "row" ) {
+                  if ( traceWidth > 0 ) {
+                    tempWidth = Math.round( itemFlex / totalFlex * traceWidth );
+                    if ( $item.isLastItem ) {
+                      tempWidth = traceWidth - sumWidth;
+                    } else {
+                      sumWidth += tempWidth;
+                    }
+                  }
+                  if ( isFillParentHeight ) {
+                    tempHeight = traceHeight;
+                  }
+                }
+                if ( direction === "column" ) {
+                  if ( traceHeight > 0 ) {
+                    tempHeight = Math.round( itemFlex / totalFlex * traceHeight );
+                    if ( $item.isLastItem ) {
+                      tempHeight = traceHeight - sumHeight;
+                    } else {
+                      sumHeight += tempHeight;
+                    }
+                  }
+                  if ( isFillParentWidth ) {
+                    tempWidth = traceWidth;
+                  }
+                } else if ( isFillParentWidth ) {
+                  if ( isFillParentWidth ) {
+                    tempWidth = traceWidth;
+                  }
+                  if ( isFillParentHeight ) {
+                    tempHeight = traceHeight;
+                  }
+                }
+                $item.uiFlex( "lock" );
+                $item.uiFlex( "render", tempWidth, tempHeight );
+                $item.uiFlex( "unlock" );
+              }
+            }
+          }
+          return this;
+        },
         noticeParent: function( ) {
           var parent = this.findParent( );
           if ( parent ) {
-            parent.uiSplitterpane( "resize" );
+            parent.uiFlex( "render" );
           }
         },
         findParent: function( ) {
-          var parent = this.target.parent( "[ui-flexpane]" );
+          var parent = this.target.parent( "[ui-flex]" );
           if ( parent.length && parent[ 0 ] === this.target[ 0 ].parentNode ) {
             return parent;
           }
@@ -180,18 +346,36 @@ aQuery.define( "ui/flex", [
         unlock: function( ) {
           this._lock = false;
         },
+        fillParent: function( ) {
+          if ( this.options.fillParentWidth ) {
+            this.target.width( this.target.parent( ).width( ) );
+          }
+          if ( this.options.fillParentHeight ) {
+            this.target.height( this.target.parent( ).height( ) );
+          }
+        },
         init: function( opt, target ) {
           this._super( opt, target );
           var self = this;
           this._lock = false;
+          this.width = 0;
+          this.height = 0;
+          this.traceWidth = 0;
+          this.traceHeight = 0;
+          if ( !this.findParent( ) ) {
+            this.fillParent( );
+          }
           setTimeout( function( ) {
             self.render( );
-          }, 1 );
+          }, 0 );
           return this;
         },
         customEventName: [ ],
         options: {
-          flex: ""
+          flex: 0,
+          flexDirection: "row",
+          fillParentWidth: true,
+          fillParentHeight: true
         },
         getter: {
 
@@ -202,7 +386,6 @@ aQuery.define( "ui/flex", [
         publics: {
           setWidth: Widget.AllowPublic,
           setHeight: Widget.AllowPublic,
-          resize: Widget.AllowPublic,
           lock: Widget.AllowPublic,
           unlock: Widget.AllowPublic
         },
@@ -214,7 +397,7 @@ aQuery.define( "ui/flex", [
       };
     }
 
-    flex = Widget.extend( "ui.flex", proto, {
+    var flex = Widget.extend( "ui.flex", proto, {
       flexName: flexName
     } );
 
