1e9e580212e740483d5b8d3fb71a8660f110e993
caojun-mac <78612846@qq.com>
2013-11-07 10:02:55 +0800

    modify build

--- build/buildjs.js
+++ build/buildjs.js
@@ -1,6 +1,38 @@
+ //需要安装 minimatch
  var buildConfig = {
-   debug: false
- }
+   debug: false,
+   amdqueryPath: '../amdquery/',
+   projectRootPath: '../../',
+   outputPath: '../output/',
+   apps: {},
+   levelOfJSMin: 3,
+   uglifyOptions: {
+     strict_semicolons: false,
+     mangle_options: {
+       mangle: true,
+       toplevel: false,
+       defines: null,
+       except: null,
+       no_functions: false
+     },
+     squeeze_options: {
+       make_seqs: true,
+       dead_code: true,
+       no_warnings: false,
+       keep_comps: true,
+       unsafe: false
+     },
+     gen_options: {
+       indent_start: 0,
+       indent_level: 4,
+       quote_keys: false,
+       space_colon: false,
+       beautify: false,
+       ascii_only: false,
+       inline_script: false
+     }
+   }
+ };
 
  var argvs = process.argv;
 
@@ -8,7 +40,7 @@
    "-d": function( ) {
      buildConfig.debug = true;
    }
- }
+ };
 
  var relativePath = process.argv[ 1 ].replace( /([\\\/])[^\\\/]*$/, '$1' );
 
@@ -17,30 +49,35 @@
    if ( !( /^\.*\//.test( buildConfigFile ) ) ) {
      buildConfigFile = './' + buildConfigFile;
    }
- } else {
-   buildConfigFile = './buildjs_config.js';
+   buildConfigFile = require( buildConfigFile );
+   for ( var i in buildConfigFile ) {
+     buildConfig[ i ] = buildConfigFile[ i ];
+   }
  }
 
- require( buildConfigFile );
 
-//Configurate oye.js
-amdqueryPath = relativePath + amdqueryPath;
 
-//Configurate project root path
-projectRootPath = relativePath + '../../';
+ var FSO = require( 'fs' );
+ var PATH = require( 'path' );
 
-//All build files will save to outputPath
-outputPath = relativePath + '../output/';
+ //Configurate oye.js
+ amdqueryPath = buildConfig.amdqueryPath;
 
-var oye = require('./lib/oye.node.js');
+ //Configurate project root path
+ projectRootPath = buildConfig.projectRootPath;
 
-oye.setPath({
-  'oyeModulePath': amdqueryPath,
-  'projectRootPath': projectRootPath
-});
-require = oye.require;
-define = oye.define;
-macthDefineOrRequire = oye.macthDefineOrRequire;
+ //All build files will save to outputPath
+ outputPath = relativePath + '../output/';
+
+ var oye = require( './lib/oye.node.js' );
+
+ oye.setPath( {
+   'oyeModulePath': amdqueryPath,
+   'projectRootPath': projectRootPath
+ } );
+ amdRequire = oye.require;
+ amdDefine = oye.define;
+ macthDefineOrRequire = oye.macthDefineOrRequire;
 
 
  for ( var i = 2, len = argvs.length, fn; i < len; i++ ) {
@@ -50,7 +87,171 @@ macthDefineOrRequire = oye.macthDefineOrRequire;
    }
  }
 
-
  var logger = buildConfig.debug ? console.info : function( ) {};
 
- logger( relativePath )
+ logger( relativePath );
+
+ //Uglify
+ var uglify = require( './lib/uglify/uglify-js.js' );
+ var minify = function( content ) {
+   return uglify( content, uglifyOptions );
+ };
+
+ var glob = require( "glob" );
+ var async = require( "./lib/async.js" );
+
+ var loadedModule = 'loaded' + ( -new Date( ) );
+ var fileStack = {};
+ fileStack[ loadedModule ] = {};
+ var baseFileStack = [ ];
+
+ function readBaseAMDQueryJS( next, result ) {
+   oye.readFile( buildConfig.amdqueryPath + "amdquery.js", function( content ) {
+     setBaseFileStack( content, "amdquery.js" );
+     next( null, content );
+   } );
+ }
+
+ function buildApps( result, next ) {
+
+   if ( !buildConfig.apps ) {
+     return next( "apps is empty", null );
+   }
+   var l = 0,
+     key;
+   for ( key in buildConfig.apps ) {
+     l++;
+
+   }
+   if ( l === 0 ) {
+     next( "apps is empty", null );
+   }
+   //Process all apps
+   var n = l,
+     appName,
+     callback = function( ) {
+       n--;
+
+       if ( n <= 0 ) {
+         next( null, null );
+       }
+     };
+
+   for ( appName in buildConfig.apps ) {
+     _build( buildConfig.apps[ appName ], appName, callback );
+   }
+
+ }
+
+ function main( ) {
+   async.waterfall( [ readBaseAMDQueryJS, buildApps ], function( err, result ) {
+     if ( err ) {
+       throw err;
+     }
+     switch ( result ) {
+
+     }
+   } );
+ }
+
+ var _build = function( sModule, appName, callback ) {
+   oye.require( sModule, function( Module ) { //Asynchronous
+     var list = Module.getDependenciesMap( );
+     var l = list.length;
+     console.info( '\u001b[34m' + '\r\nDependencies length of module ' + sModule + ': ' + l + '\u001b[39m' );
+     var item, moduleName, n = l,
+       fn = function( ) {
+         n--;
+         if ( n === 0 ) {
+           if ( callback )
+             callback( );
+         }
+       }
+       console.log(list)
+     for ( var i = 0; i < l; i++ ) {
+       item = list[ i ];
+       moduleName = item.name;
+       console.log( moduleName, item.path, oye.require.content[moduleName], appName );
+       // processFile( moduleName, module.path, appName, fn );
+     }
+   } );
+ };
+
+ function processFile( module, path, target, callbak ) {
+   FSO.readFile( path, function( err, data ) {
+     if ( err ) {
+       throw err;
+     }
+     var content = data.toString( );
+
+     setFileStack( content, module, target );
+
+     if ( callback ) {
+       callback( content );
+     }
+   } );
+ }
+
+ function editDefine( content, module ) {
+   content = "/*===================" + module + "===========================*/\n" + content;
+   var r = /define\s*\(\s*(['"])[^\1]*\1/i;
+   if ( !r.test( content ) ) {
+     content = content.replace( /define\(/, 'define("' + module + '",' );
+   }
+   content += "\n/*=======================================================*/\n";
+   logger( content );
+   return content;
+ }
+
+ function setBaseFileStack( content, module ) {
+   content = editDefine( content, module );
+   baseFileStack.unshift( content );
+ }
+
+ function setFileStack( content, module, target ) {
+   content = editDefine( content, module );
+   if ( !fileStack[ target ] ) {
+     fileStack[ target ] = {
+       modules: [ ],
+       content: [ ]
+     };
+   }
+   if ( !fileStack[ loadedModule ][ module ] ) {
+     fileStack[ loadedModule ][ module ] = true;
+     fileStack[ target ].modules.unshift( module );
+     fileStack[ target ].content.unshift( content );
+   }
+ }
+
+ function mkdirSync( path ) {
+   if ( !path || PATH.existsSync( path ) ) {
+     return;
+   }
+   var r = /(?=[\\\/]+)/;
+   var list = path.split( r );
+   var l = list.length;
+   var _path;
+   for ( var i = 1; i < l; i++ ) {
+     _path = list.slice( 0, i ).join( '' );
+     if ( PATH.existsSync( _path ) ) {
+       continue;
+     }
+     FSO.mkdirSync( _path );
+   }
+ }
+
+ function forEach( obj, callback, context ) {
+   if ( !obj ) return this;
+   var i = 0,
+     item, len = obj.length,
+     isObj = typeof len != "number" || typeof obj == "function";
+   if ( isObj ) {
+     for ( item in obj )
+       if ( callback.call( context || obj[ item ], obj[ item ], item ) === false ) break;
+   } else
+     for ( var value = obj[ 0 ]; i < len && callback.call( context || value, value, i ) !== false; value = obj[ ++i ] ) {}
+   return this;
+ }
+
+
+ main( );
--- build/buildjs_config.js
+++ build/buildjs_config.js
@@ -1,24 +1,19 @@
 //Configurate oye.js
-amdqueryPath = '../amdquery/';
+exports.amdqueryPath = '../amdquery/';
 
 //Configurate project root path
-projectRootPath = '../../';
+exports.projectRootPath = '../../';
 
 //All build files will save to outputPath
-outputPath = '../output/';
+exports.outputPath = '../output/';
 
 //All apps entrance - get them from app/*.js(only one segment)
-apps = [
-
-];
-
-defines = [
-
-];
-
+exports.apps = {
+    "test": "../build/test"
+}
 
 //Compress level of JS Minifier
-levelOfJSMin = 3;
+exports.levelOfJSMin = 3;
 /*
 minimal:1,//ratio:75%
 conservative:2,//ratio:68%
@@ -26,29 +21,29 @@ agressive:3//ratio:67%
 */
 
 //Compress configuration of UglifyJS
-uglifyOptions = {
-    strict_semicolons: false,
-    mangle_options: {
-        mangle       : true,
-        toplevel     : false,
-        defines      : null,
-        except       : null,
-        no_functions : false
-    },
-    squeeze_options: {
-        make_seqs   : true,
-        dead_code   : true,
-        no_warnings : false,
-        keep_comps  : true,
-        unsafe      : false
-    },
-    gen_options: {
-        indent_start : 0,
-        indent_level : 4,
-        quote_keys   : false,
-        space_colon  : false,
-        beautify     : false,
-        ascii_only   : false,
-        inline_script: false
-    }
+exports.uglifyOptions = {
+  strict_semicolons: false,
+  mangle_options: {
+    mangle: true,
+    toplevel: false,
+    defines: null,
+    except: null,
+    no_functions: false
+  },
+  squeeze_options: {
+    make_seqs: true,
+    dead_code: true,
+    no_warnings: false,
+    keep_comps: true,
+    unsafe: false
+  },
+  gen_options: {
+    indent_start: 0,
+    indent_level: 4,
+    quote_keys: false,
+    space_colon: false,
+    beautify: false,
+    ascii_only: false,
+    inline_script: false
+  }
 };
