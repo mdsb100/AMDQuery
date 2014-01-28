b2de8858a935a0867c838a4def420fd3513ff44d
caojun-mac <78612846@qq.com>
2014-01-28 22:46:28 +0800

    document main/class

--- amdquery/main/class.js
+++ amdquery/main/class.js
@@ -1,69 +1,87 @@
 ﻿aQuery.define( "main/class", [ "base/extend", "base/support" ], function( $, utilExtend, support, undefined ) {
 	"use strict";
-	var cls,
-		replaceClass = function( ele, oldClassName, newClassName ) {
-			/// <summary>清空所有样式表</summary>
-			/// <param name="ele" type="Element">ele元素</param>
-			/// <param name="className" type="String">替换整个样式表 缺省为空</param>
-			/// <returns type="self" />
-			oldClassName && ( ele.className = ele.className.replace( oldClassName, newClassName ) );
-			return this;
-		};
+	/**
+	 * @name replaceClass
+	 * @method
+	 * @param {Element} ele
+	 * @param {String} oldClassName
+	 * @param {String} newClassName
+	 * @returns {this}
+	 */
+	function replaceClass( ele, oldClassName, newClassName ) {
+		oldClassName && ( ele.className = ele.className.replace( oldClassName, newClassName ) );
+		return this;
+	};
+
+	/**
+	 * @pubilc
+	 * @exports main/class
+	 * @requires module:base/extend
+	 * @requires module:base/support
+	 * @borrows replaceClass as replaceClass
+	 */
+	var cls;
+
 	if ( support.classList ) {
-		cls = {
+		cls = /** @lends module:main/class */ {
+			/**
+			 * @param {Element}
+			 * @param {String}
+			 * @returns {this}
+			 */
 			addClass: function( ele, className ) {
-				/// <summary>给DOM元素添加样式表</summary>
-				/// <param name="ele" type="Element">ele元素</param>
-				/// <param name="className" type="String">样式表</param>
-				/// <returns type="self" />
 				className != "" && ele.classList.add( className );
 				return this;
 			},
+			/**
+			 * @param {Element}
+			 * @param {String}
+			 * @returns {Boolean}
+			 */
 			containsClass: function( ele, className ) {
-				/// <summary>获得指定的DOM元素的样式名</summary>
-				/// <param name="ele" type="Element">dom元素</param>
-				/// <param name="className" type="String">样式名</param>
-				/// <returns type="Boolean" />
 				return ele.classList.contains( className );
 			},
+			/**
+			 * @param {Element}
+			 * @param {String}
+			 * @returns {this}
+			 */
 			removeClass: function( ele, className ) {
-				/// <summary>对元素删除一个样式类</summary>
-				/// <param name="ele" type="Object">对象</param>
-				/// <param name="className" type="String">样式名</param>
-				/// <returns type="self" />
 				className != "" && ele.classList.remove( className );
 				return this;
 			},
+			/**
+			 * If className exists then remove, if className does not exists then add.
+			 * @param {Element}
+			 * @param {String}
+			 * @returns {this}
+			 */
 			toggleClass: function( ele, className ) {
-				/// <summary>切换元素样式</summary>
-				/// <param name="ele" type="Object">对象</param>
-				/// <param name="className" type="String">样式名</param>
-				/// <returns type="self" />
 				className != "" && ele.classList.toggle( className );
 				return this;
 			},
 			replaceClass: replaceClass,
+			/**
+			 * return class length.
+			 * @param {Element}
+			 * @returns {Number}
+			 */
 			classLength: function( ele ) {
-				/// <summary>获得Class的个数</summary>
-				/// <param name="ele" type="Object">对象</param>
-				/// <returns type="Number" />
 				return ele.classList.length;
 			},
+			/**
+			 * If className exists then remove, if className does not exists then add.
+			 * @param {Element}
+			 * @param {Number}
+			 * @returns {String}
+			 */
 			getClassByIndex: function( ele, index ) {
-				/// <summary>获得样式在元素的索引</summary>
-				/// <param name="ele" type="Object">对象</param>
-				/// <param name="index" type="Number">样式名</param>
-				/// <returns type="String" />
 				return ele.classList.item( index );
 			}
 		};
 	} else {
 		cls = {
 			addClass: function( ele, className ) {
-				/// <summary>给DOM元素添加样式表</summary>
-				/// <param name="ele" type="Element">ele元素</param>
-				/// <param name="className" type="String">样式表</param>
-				/// <returns type="self" />
 				if ( !$.containsClass( ele, className ) ) {
 					var str = " ";
 					if ( ele.className.length == 0 ) str = "";
@@ -73,19 +91,11 @@
 				return this;
 			},
 			containsClass: function( ele, className ) {
-				/// <summary>获得指定的DOM元素的样式名</summary>
-				/// <param name="ele" type="Element">dom元素</param>
-				/// <param name="className" type="String">样式名</param>
-				/// <returns type="String" />
 				var reg = new RegExp( "(\\s|^)" + className + "(\\s|$)" ),
 					result = ele.className.match( reg );
 				return !!( result && result[ 0 ] );
 			},
 			removeClass: function( ele, className ) {
-				/// <summary>对元素删除一个样式类</summary>
-				/// <param name="ele" type="Object">对象</param>
-				/// <param name="className" type="String">样式名</param>
-				/// <returns type="self" />
 				if ( $.containsClass( ele, className ) ) {
 					var reg = new RegExp( "(\\s|^)" + className + "(\\s|$)" );
 					ele.className = ele.className.replace( reg, " " );
@@ -93,25 +103,14 @@
 				return this;
 			},
 			toggleClass: function( ele, className ) {
-				/// <summary>切换元素样式</summary>
-				/// <param name="ele" type="Object">对象</param>
-				/// <param name="className" type="String">样式名</param>
-				/// <returns type="self" />
 				$.containsClass( ele, className ) ? $.removeClass( ele, className ) : $.addClass( ele, className );
 				return this;
 			},
 			replaceClass: replaceClass,
 			classLength: function( ele ) {
-				/// <summary>获得Class的个数</summary>
-				/// <param name="ele" type="Object">对象</param>
-				/// <returns type="Number" />
 				return ( $.util.trim( ele.className ).split( " " ) ).length;
 			},
 			getClassByIndex: function( ele, index ) {
-				/// <summary>获得样式在元素的索引</summary>
-				/// <param name="ele" type="Object">对象</param>
-				/// <param name="index" type="Number">样式名</param>
-				/// <returns type="String" />
 				return ( $.util.trim( ele.className ).split( " " ) )[ index ] || null;
 			}
 		};
@@ -120,55 +119,76 @@
 	$.extend( cls );
 
 	$.fn.extend( {
+		/**
+		 * @public
+		 * @memberof aQuery.prototype
+		 * @param {String}
+		 * @returns {this}
+		 */
 		addClass: function( className ) {
-			/// <summary>给所有DOM元素添加样式表</summary>
-			/// <param name="className" type="String">样式表</param>
-			/// <returns type="self" />
 			return this.each( function( ele ) {
-				$.addClass( ele, className );
+				cls.addClass( ele, className );
 			}, this );
 		},
+    /**
+     * @public
+     * @memberof aQuery.prototype
+     * @param {String}
+     * @returns {Boolean}
+     */
 		containsClass: function( className ) {
-			/// <summary>第一个元素是否有个样式名</summary>
-			/// <param name="className" type="String">样式名</param>
-			/// <returns type="Boolean" />
-			return $.containsClass( this[ 0 ], className );
+			return cls.containsClass( this[ 0 ], className );
 		},
+    /**
+     * @public
+     * @memberof aQuery.prototype
+     * @param {String}
+     * @returns {this}
+     */
 		removeClass: function( className ) {
-			/// <summary>对所有元素删除一个样式类</summary>
-			/// <param name="className" type="String">样式名</param>
-			/// <returns type="self" />
 			return this.each( function( ele ) {
-				$.removeClass( ele, className );
+				cls.removeClass( ele, className );
 			} );
 		},
+    /**
+     * @public
+     * @memberof aQuery.prototype
+     * @param {String}
+     * @returns {this}
+     */
 		toggleClass: function( className ) {
-			/// <summary>切换元素样式</summary>
-			/// <param name="className" type="String">样式名</param>
-			/// <returns type="Number" />
 			return this.each( function( ele ) {
-				$.toggleClass( ele, className );
+				cls.toggleClass( ele, className );
 			} );
 		},
+    /**
+     * @public
+     * @memberof aQuery.prototype
+     * @param {String}
+     * @param {String}
+     * @returns {this}
+     */
 		replaceClass: function( oldClassName, newClassName ) {
-			/// <summary>替换元素所有样式</summary>
-			/// <param name="className" type="String">样式名</param>
-			/// <returns type="self" />
 			return this.each( function( ele ) {
-				$.replaceClass( ele, oldClassName, newClassName );
+				cls.replaceClass( ele, oldClassName, newClassName );
 			} );
 		},
+    /**
+     * @public
+     * @memberof aQuery.prototype
+     * @returns {Number}
+     */
 		classLength: function() {
-			/// <summary>获得Class的个数</summary>
-			/// <returns type="Number" />
-			return $.classLength( this[ 0 ] );
+			return cls.classLength( this[ 0 ] );
 		},
+    /**
+     * @public
+     * @memberof aQuery.prototype
+     * @param {Number}
+     * @returns {String}
+     */
 		getClassByIndex: function( index ) {
-			/// <summary>获得样式在元素的索引</summary>
-			/// <param name="ele" type="Object">对象</param>
-			/// <param name="index" type="Number">样式名</param>
-			/// <returns type="String" />
-			return $.getClassByIndex( this[ 0 ], index );
+			return cls.getClassByIndex( this[ 0 ], index );
 		}
 	} );
 
