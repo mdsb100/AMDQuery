4c57417fca9f8822f0dfba81de20dec39cde9787
caojun-mac <78612846@qq.com>
2014-01-31 19:08:05 +0800

    document applicationCache

--- amdquery/html5/applicationCache.js
+++ amdquery/html5/applicationCache.js
@@ -1,77 +1,94 @@
-﻿aQuery.define( "html5/applicationCache", function( $, undefined ) {
+﻿define( "html5/applicationCache", function( undefined ) {
 	"use strict";
-  this.describe("HTML5 applicationCache");
+	this.describe( "HTML5 applicationCache" );
+
+	/**
+	 * @pubilc
+	 * @exports html5/applicationCache
+	 */
+	var applicationCache = null;
+
 	if ( window.applicationCache ) {
-		var aCache = applicationCache;
-		$.applicationCache = {
+		var aCache = window.applicationCache;
+
+		applicationCache = /** @lends module:html5/applicationCache */ {
+			/**
+			 * @param {String} - Event name.
+			 * @param {Function}
+			 * @returns {this}
+			 */
 			addEventListener: function( type, fun ) {
-				/// <summary>绑定applicationCache事件</summary>
-				/// <param name="type" type="String">事件名</param>
-				/// <param name="fun" type="Function">方法</param>
-				/// <returns type="self" />
 				aCache.addEventListener( type, fun, true );
 				return this;
 			},
+			/**
+			 * @returns {this}
+			 */
 			swapCache: function() {
-				/// <summary>手动更新</summary>
-				/// <returns type="self" />
 				aCache.swapCache();
 				return this;
 			},
+			/**
+			 * @param {String} - Event name.
+			 * @param {Function}
+			 * @returns {this}
+			 */
 			removeEventListener: function( type, fun ) {
-				/// <summary>解除applicationCache事件</summary>
-				/// <param name="type" type="String">事件名</param>
-				/// <param name="fun" type="Function">方法</param>
-				/// <returns type="self" />
 				aCache.removeEventListener( type, fun );
 				return this;
 			},
-
+			/**
+			 * @param {Function}
+			 * @returns {this}
+			 */
 			checking: function( fun ) {
-				/// <summary>检查</summary>
-				/// <param name="fun" type="Function">方法</param>
-				/// <returns type="self" />
 				return this.addEventListener( arguments[ 1 ] || "checking", function() {
 					fun.apply( this, arguments );
 				} );
 			},
+			/**
+			 * @param {Function}
+			 * @returns {this}
+			 */
 			noupdate: function( fun ) {
-				/// <summary>无更新</summary>
-				/// <param name="fun" type="Function">方法</param>
-				/// <returns type="self" />
 				return this.checking( fun, "noupdate" );
 			},
+			/**
+			 * @param {Function}
+			 * @returns {this}
+			 */
 			downloading: function( fun ) {
-				/// <summary>下载</summary>
-				/// <param name="fun" type="Function">方法</param>
-				/// <returns type="self" />
 				return this.checking( fun, "downloading" );
 			},
+			/**
+			 * @param {Function}
+			 * @returns {this}
+			 */
 			progress: function( fun ) {
-				/// <summary>进度</summary>
-				/// <param name="fun" type="Function">方法</param>
-				/// <returns type="self" />
 				return this.checking( fun, "progress" );
 			},
+			/**
+			 * @param {Function}
+			 * @returns {this}
+			 */
 			updateready: function( fun ) {
-				/// <summary>更新完毕</summary>
-				/// <param name="fun" type="Function">方法</param>
-				/// <returns type="self" />
 				return this.checking( fun, "updateready" );
 			},
+			/**
+			 * @param {Function}
+			 * @returns {this}
+			 */
 			cached: function( fun ) {
-				/// <summary>已缓存</summary>
-				/// <param name="fun" type="Function">方法</param>
-				/// <returns type="self" />
 				return this.checking( fun, "cached" );
 			},
+			/**
+			 * @param {Function}
+			 * @returns {this}
+			 */
 			error: function( fun ) {
-				/// <summary>错误</summary>
-				/// <param name="fun" type="Function">方法</param>
-				/// <returns type="self" />
 				return this.checking( fun, "error" );
 			}
 		};
 	}
-	return $.applicationCache || null;
+	return applicationCache;
 } );
