be7132268d78b1a3ccdd8482e290a1bdd7907e77
caojun-mac <78612846@qq.com>
2014-03-01 01:00:07 +0800

    modify jsdoc

--- jsdoc/makedoc.sh
+++ jsdoc/makedoc.sh
@@ -1,3 +1,3 @@
-api_path=./../document/app/assets/api/
+api_path=./../document/assets/api/
 rm -fr $api_path
 jsdoc ../amdquery/ ../amdquery/**/*.js --template templates/docstrap --destination $api_path
