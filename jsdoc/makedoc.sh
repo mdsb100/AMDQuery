efda9328d8832b5f6a5678e35b410929f2b99b1e
caojun-mac <78612846@qq.com>
2014-03-01 01:16:07 +0800

    remove docstrap

--- jsdoc/makedoc.sh
+++ jsdoc/makedoc.sh
@@ -1,3 +1,3 @@
 api_path=./../document/assets/api/
 rm -fr $api_path
-jsdoc ../amdquery/ ../amdquery/**/*.js --template templates/docstrap --destination $api_path
+jsdoc ../amdquery/ ../amdquery/**/*.js --template templates/amdquery --destination $api_path
