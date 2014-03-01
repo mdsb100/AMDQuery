b0edcfd0fd1e9e538e1656df25fc82992371ffdd
caojun-mac <78612846@qq.com>
2014-03-01 19:56:32 +0800

    fix test

---
+++ test/lib/aQueryConfig.js
@@ -0,0 +1,16 @@
+var aQueryConfig = {
+	amdquery: {
+		define: "$",
+		loadingImage: "assets/images/welcome.gif"
+	},
+	amd: {
+		detectCR: 1
+	},
+	module: {
+		compatibleEvent: 1
+	},
+	app: {
+		src: "../document/app",
+		debug: 1
+	}
+};
