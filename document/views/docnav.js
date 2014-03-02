57afdf3770d5de93f9f1e5264c48986b7cf0d13f
caojun-mac <78612846@qq.com>
2014-03-02 20:52:27 +0800

    remove docnav.css

--- document/views/docnav.js
+++ document/views/docnav.js
@@ -1,7 +1,6 @@
 aQuery.define( "@app/views/docnav", [ "base/client", "main/css", "app/View", "ui/flex", "ui/scrollableview", "ui/navmenu", "ui/navitem" ], function( $, client, css, SuperView ) {
 	"use strict"; //启用严格模式
 	var xmlpath = "@app/xml/docnav";
-	SuperView.getStyle( "@app/styles/docnav" );
 
 	var View = SuperView.extend( {
 		init: function( contollerElement ) {
