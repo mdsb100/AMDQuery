243004a3b93db31f2a933fa5977e4ac44461f77e
caojun-mac <78612846@qq.com>
2014-04-13 18:23:52 +0800

    Add testRefer.

--- test/app.js
+++ test/app.js
@@ -1,4 +1,4 @@
-aQuery.define( "@app/app", [ "app/Application", "@app/controllers/index" ], function( $, Application ) {
+aQuery.define( "@app/app", [ "app/Application", "@app/controllers/index", "@app/lib/testRefer" ], function( $, Application ) {
   "use strict"; //启用严格模式
   //必须依赖index controller
   var app = Application.extend( "TestApplication", {
---
+++ test/lib/testRefer.js
@@ -0,0 +1,17 @@
+aQuery.define( "@app/lib/testRefer", [
+    "base/Promise",
+
+    "main/CustomEvent",
+    "main/position",
+    "main/query",
+    "main/communicate",
+    "main/parse",
+    "main/class",
+    "main/dom",
+    "main/event",
+    "main/object",
+    "main/data",
+
+    "module/Test",
+    "module/location"
+  ], {} );
