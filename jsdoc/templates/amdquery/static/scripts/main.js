dd887905981d7f597316a841a04998924751ded0
caojun-mac <78612846@qq.com>
2014-06-25 00:03:39 +0800

    Fix main.js.

--- jsdoc/templates/amdquery/static/scripts/main.js
+++ jsdoc/templates/amdquery/static/scripts/main.js
@@ -1,39 +1,41 @@
 $.require( [ "main/event", "main/query", "main/attr", "module/location", "ui/scrollableview" ], function( event, query, attr, location ) {
 
-	$( ".prettyprinted" ).each( function( ele ) {
-		var numbered = ele.innerHTML.split( "\n" );
-		var counter = 0;
+  $( ".prettyprinted" ).each( function( ele ) {
+    var numbered = ele.innerHTML.split( "\n" );
+    var counter = 0;
+    if ( numbered === 1 ) {
+      debugger
+    };
+    for ( var i = 0, len = numbered.length, item, html = ""; i < len; i++ ) {
+      item = numbered[ i ];
+      html += i != len - 1 || len === 1 ? '<span class="linenumber" id="line' + ( i + 1 ) + '" >' + ( i + 1 ) + "</span>" + item + "\n" : "";
+    }
 
-		for ( var i = 0, len = numbered.length, item, html = ""; i < len; i++ ) {
-			item = numbered[ i ];
-			html += i != len - 1 ? '<span class="linenumber" id="line' + ( i + 1 ) + '" >' + ( i + 1 ) + "</span>" + item + "\n" : "";
-		}
+    ele.innerHTML = html;
+  } );
 
-		ele.innerHTML = html;
-	} );
+  var hash = location.getHash( "#" );
 
-	var hash = location.getHash( "#" );
+  if ( hash ) {
+    var target = $( "#main" ).uiScrollableview( "getAnimationToElementById", hash );
+    if ( target ) {
+      $( "#main" ).uiScrollableview( "animateToElement", target );
+    }
+  }
 
-	if ( hash ) {
-		var target = $( "#main" ).uiScrollableview( "getAnimationToElementById", hash );
-		if ( target ) {
-			$( "#main" ).uiScrollableview( "animateToElement", target );
-		}
-	}
-
-	$( "body" ).delegate( "a", "click", function( e ) {
-		if ( window.parent && window.parent.aQuery && window.parent.aQuery.trigger ) {
-			var a = event.document.getTarget( e ),
-				href = attr.getAttr( a, "href" );
-			if ( href ) {
-				var type = "api_iframe.hrefChange";
-				window.parent.aQuery.trigger( type, null, {
-					type: type,
-					href: href,
-					target: a,
-				} );
-			}
-		}
-	} );
+  $( "body" ).delegate( "a", "click", function( e ) {
+    if ( window.parent && window.parent.aQuery && window.parent.aQuery.trigger ) {
+      var a = event.document.getTarget( e ),
+        href = attr.getAttr( a, "href" );
+      if ( href ) {
+        var type = "api_iframe.hrefChange";
+        window.parent.aQuery.trigger( type, null, {
+          type: type,
+          href: href,
+          target: a,
+        } );
+      }
+    }
+  } );
 
 } );
